4dffaa5d50553ec8d14b66945b60e853bf4208e5
Glitch (glitch-hello-website) <none>
2024-10-21 22:23:26 +0000

    🍖👯 Checkpoint ./main.js:44197544/49 ./gallery.html:44197544/82 ./style.css:44197544/172

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.offscreen-menu');
 
 hamMenu.addEventListener('click', () => {
-  hamMenu.classList.toggle('active')
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
 })
