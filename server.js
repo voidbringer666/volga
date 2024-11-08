afc6adf883f9539cc7b75afdc7c1f763a85cba34
Glitch (glitch-hello-website) <none>
2024-11-08 14:16:03 +0000

    🎙📌 Checkpoint ./server.js:44197544/617

---
+++ server.js
@@ -0,0 +1,19 @@
+// declare variables
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer'); //optional if I want to send email
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public')); //serve static files (css, js)
+
+//Route to handle form submissions
+app.post('/contact', (req, res) => {
+  const { name, email, message } = req.body;
+  
+  //optional: send email using nodemailer, log it to console, 
+})
